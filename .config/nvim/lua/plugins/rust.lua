7e4efe2dec29a4013d9737781a7c9c121d3fe9a6
Mateusz Kurzyna <lolc44t@gmail.com>
2026-05-13 18:46:20 +0200

    nvim: configure dap for rust

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -12,6 +12,7 @@ return {
 					},
 				},
 				server = {},
+				dap = {},
 			}
 		end,
 	},
@@ -20,10 +21,45 @@ return {
 		event = { "BufRead Cargo.toml" },
 		config = true,
 	},
+	{
+		"mfussenegger/nvim-dap",
+		optional = true,
+		opts = function()
+			local dap = require("dap")
 
-	-- {
-	--   "mrcjkb/rustaceanvim",
-	--   version = "^6", -- Recommended
-	--   lazy = false, -- This plugin is already lazy
-	-- }
+			-- Ensure codelldb adapter is registered
+			if not dap.adapters.codelldb then
+				dap.adapters.codelldb = {
+					type = "server",
+					port = "${port}",
+					executable = {
+						command = vim.fn.exepath("codelldb"),
+						args = { "--port", "${port}" },
+					},
+				}
+			end
+
+			-- Add "attach to process" configuration for Rust
+			dap.configurations.rust = dap.configurations.rust or {}
+			table.insert(dap.configurations.rust, {
+				name = "Attach to process",
+				type = "codelldb",
+				request = "attach",
+				pid = require("dap.utils").pick_process,
+				args = {},
+			})
+		end,
+		-- stylua: ignore
+		keys = {
+			{ "<leader>dK", function()
+				require("dap").run({
+					name = "Attach to process",
+					type = "codelldb",
+					request = "attach",
+					pid = require("dap.utils").pick_process,
+				})
+			end, ft = "rust", desc = "Attach to Process (Rust)" },
+		},
+	},
 }
+
